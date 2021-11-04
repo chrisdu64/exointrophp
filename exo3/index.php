b825c491653fdc6aa4f848eab820e026c3fefd44
chris <christophedeslandes@hotmail.fr>
2021-11-04 16:35:19 +0100

    fin exo4 à 6

--- exo3/index.php
+++ exo3/index.php
@@ -1,5 +1,17 @@
 <?php
-
+#exo 4
     for($i=1; $i<26;$i++){
         echo $i."<br>";
     }
+#exo 5
+    for($i=25; $i>0;$i--){
+        echo $i."<br>";
+    }
+#exo 6
+    $n=0;
+    for($i=1; $i<26;$i++){
+        $n=$n.$i;        
+        echo $n."<br>";
+        }
+              
+    
---
+++ exo4/index.php
@@ -0,0 +1,8 @@
+<?php
+    $nombre = 0;
+
+    for($i=1;$i<=30;$i++){
+        $nombre+=$i;
+        echo 'La somme des nombres est de '.$nombre.'<br>';
+    }
+    
