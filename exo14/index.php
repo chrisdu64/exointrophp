b1ff4d05cd30667359da628ffc1d39f8a73d839a
chris <christophedeslandes@hotmail.fr>
2021-11-05 12:38:32 +0100

    definitif

--- exo14/index.php
+++ exo14/index.php
@@ -1,10 +1,10 @@
 <?php
 
-    $nombre=array(2721,34245,64444,24348,43483,1844,44861,68651,56956,98414);
-    $petit=array();
-    for($i=0;$i<=count($nombre);$i++){
+    $nombre=[2721,34245,64444,24348,43483,1844,44861,68651,56956,98414];
+    $petit=9223372036854775807;
+    for($i=0;$i< count($nombre);$i++){
         if ($petit > $nombre[$i]){
-            $petit= $nombre[$i];
+            $petit = $nombre[$i];
         }
     }
     echo $petit;
