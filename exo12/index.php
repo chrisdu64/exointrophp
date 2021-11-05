7c7e8f6d0c140d209185681b8621cf0e8f0ab22f
chris <christophedeslandes@hotmail.fr>
2021-11-05 11:57:17 +0100

    rectificatifs exo12

--- exo12/index.php
+++ exo12/index.php
@@ -1,9 +1,9 @@
 <?php
 
-    $nombre=347872;
+function changeTime($nombre){    
     $seconde=$nombre%60;
     $minute=(($nombre-$seconde)/60)%60;    
     $heure=((($nombre-$seconde)/60)-$minute)/60;
-    /*
-    $heure=$minute%24;*/
-    echo 'Ce nombre : '.$nombre.' correspond à : '.$heure.'h'.$minute.'m'.$seconde.'s.';
+    return 'Ce nombre : '.$nombre.' correspond à : '.$heure.'h'.$minute.'m'.$seconde.'s.';
+}
+echo changeTime(347872);    
